4cbf052e12df2c3fe2443edae5571f3e7ec5f057
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-03-20 11:56:24 -0300

    Criado a classe categoria

---
+++ farmacia/src/main/java/com/farmacia/model/Categoria.java
@@ -0,0 +1,40 @@
+package com.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categorias")
+
+public class Categoria {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O atributo nome é obrigatório")
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
